2729e1e737fec370a151cca2aa4059fb730e84e1
Karlelisa <karlelisaw@gmail.com>
2019-10-25 22:09:05 -0400

    Adding Media Queries

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 
     // Initial array of topics
     let topics = ["Beyonce", "Jay-Z", "Tupac", "Justin Timberlake", "Jennifer Lopez", "Rihanna", "Whitney Houston", "Micheal Jackson",
-        "DJ Snake", "Ariana Grande", "Katy Perry", "Bruno Mars", "Kanye West", "Khalid", "Halsey", "Imagin Dragons", "Cardi B", "Mariah Carey"];
+        "DJ Snake", "Ariana Grande", "Katy Perry", "Bruno Mars", "Kanye West", "Khalid", "Mariah Carey"];
 
 
     // Function for dumping the JSON content for each button into the div
@@ -14,9 +14,7 @@ $(document).ready(function () {
         let person = $(this).attr("data-person")
         let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=zU2CODu6jPmisRlsMJnncF52J89fLzoK&limit=10";
 
-        //https://api.giphy.com/v1/gifs/search?q=&api_key=zU2CODu6jPmisRlsMJnncF52J89fLzoK&limit=10
-
-
+        //The ajax request code to get data from the above url
         $.ajax({
             url: queryURL,
             method: "GET"
@@ -28,6 +26,7 @@ $(document).ready(function () {
                 //deleting the 10 set of gifs so that only the new music artists' gifs appear per click
                 $("#gifs-appear-here").empty();
 
+                //For loop of adding the gifs and the ratings of them
                 for (let i = 0; i < results.length; i++) {
 
                     let gifDiv = $("<div>");
@@ -41,7 +40,7 @@ $(document).ready(function () {
                     celebImage.addClass("gif")
 
                     //Cited: https://stackoverflow.com/questions/44298501/how-to-pause-and-start-gif-using-jquery-ajax
-                    //From the above site, I learned to add the word still in the below code to make the gift non-animated
+                    //From the above site, I learned to add the word still before the .url to make the gift non-animated
                     celebImage.attr("src", results[i].images.fixed_height_still.url);
                     //celebImage.attr("src", results[i].images.fixed_height.url);
 
@@ -50,26 +49,21 @@ $(document).ready(function () {
                     gifDiv.prepend(p);
 
                     $("#gifs-appear-here").prepend(gifDiv);
-
-                    //gifDiv.clear();
                 }
             });
     }
 
     //Cited: https://stackoverflow.com/questions/44298501/how-to-pause-and-start-gif-using-jquery-ajax
-    //This function plays and stops the gif with every click. When the user clicks on the non-animated gif, the gif will start to animate. When the user clicks on it again, the gif will stop animating.
-    /* This function plays and stops the gif with every click. The hasClass() checks if the div element has the class of playing or not and 
-    will return true or false. If it does have the class, it will return true and will play (animate) the gif. If the element does not have 
-    the class then it will return false and not play (animate) the gif. */
+    //This function plays and stops the gif with every click. It toggles gif play on click
     $('div').on('click', '.gif', function () {
         let src = $(this).attr("src");
 
         if ($(this).hasClass('playing')) {
-            //stop - This stops the gif from animating by removing the playing class
+            //This stops the gif from animating by removing the playing class
             $(this).attr('src', src.replace(/\.gif/i, "_s.gif"))
             $(this).removeClass('playing');
         } else {
-            //play - With another click, this code will animate the gif by adding the play class.
+            //With another click, this code will animate the gif by adding the play class.
             $(this).addClass('playing');
             $(this).attr('src', src.replace(/\_s.gif/i, ".gif"))
         }
@@ -80,17 +74,14 @@ $(document).ready(function () {
     function renderButtons() {
 
         // Deleting the buttons prior to adding new topics
-        // (this is necessary otherwise you will have repeat buttons)
         $("#buttons-view").empty();
 
         // Looping through the array of topics
         for (let i = 0; i < topics.length; i++) {
 
-
-            // Then dynamicaly generating buttons for each celeb in the array
-            // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+            //Dynamicaly generating buttons for each celeb in the array
             let a = $("<button>");
-            // Adding a class of movie to our button
+            // Adding a class of celeb to the button
             a.addClass("celeb");
             // Adding a data-attribute
             a.attr("data-person", topics[i]);
@@ -108,15 +99,15 @@ $(document).ready(function () {
         // This line grabs the input from the textbox
         let celeb = $("#celeb-input").val().trim();
 
-        // The movie from the textbox is then added to our array
+        // The celeb from the textbox is then added to our array
         topics.push(celeb);
 
-        // Calling renderButtons which handles the processing of our movie array
+        // Calling renderButtons which handles the processing of the celeb array
         renderButtons();
 
     });
 
-    // Generic function for displaying the movieInfo
+    // Click function for displaying the celeb info
     $(document).on("click", ".celeb", displayCelebGif);
 
     // Calling the renderButtons function to display the intial buttons
